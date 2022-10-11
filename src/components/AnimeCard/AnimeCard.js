8b26dd30f297b237f94e9fcaf3274bfa0bffcc46
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-10-11 22:12:09 +0100

    configure popover on hover over each anime card

--- src/components/AnimeCard/AnimeCard.js
+++ src/components/AnimeCard/AnimeCard.js
@@ -2,7 +2,19 @@ import React, { useState } from 'react';
 
 import './AnimeCard.css';
 
-import { Popover } from 'antd';
+import { HeartOutlined } from '@ant-design/icons';
+
+import {
+    Popover,
+    PopoverTrigger,
+    PopoverContent,
+    PopoverHeader,
+    PopoverBody,
+    PopoverFooter,
+    PopoverArrow,
+  } from '@chakra-ui/react'
+
+  
 
 const AnimeCard = ({ anime }) => {
 
@@ -23,19 +35,41 @@ const AnimeCard = ({ anime }) => {
         </div>
     )
 
+    const initialFocusRef = React.useRef()
+
   return (
-    <Popover
-        overlayStyle={{maxWidth: '300px'}}
-        color="#2f385f"
-        content={popoverContent} 
-        Title={anime.attributes.canonicalTitle} 
-        placement="right"
-        mouseEnterDelay="0.3"
-        trigger="hover"
-        open={show}
-        onMouseLeave={() => setShow(false)}
-    >
-        <div className='anime-card'>
+    // <Popover
+    //     overlayStyle={{maxWidth: '300px'}}
+    //     color="#2f385f"
+    //     content={popoverContent} 
+    //     Title={anime.attributes.canonicalTitle} 
+    //     placement="right"
+    //     mouseEnterDelay="0.3"
+    //     trigger="hover"
+    //     open={show}
+    //     onMouseLeave={() => setShow(false)}
+    // >
+        // <div className='anime-card'>
+        //     <a 
+        //         className='darken'
+        //         onMouseLeave={() => setShow(false)}
+        //         onMouseEnter={() => setShow(true)}
+        //     >
+        //         <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
+        //     </a>
+        // </div>
+    // </Popover>
+
+    <div>
+        <Popover
+            initialFocusRef={initialFocusRef}
+            placement='bottom'
+            closeOnBlur={true}
+            trigger="hover"
+            className='popover-box'
+        >
+            <PopoverTrigger>
+            <div className='anime-card'>
             <a 
                 className='darken'
                 onMouseLeave={() => setShow(false)}
@@ -43,8 +77,42 @@ const AnimeCard = ({ anime }) => {
             >
                 <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
             </a>
-        </div>
-    </Popover>
+            </div>
+            </PopoverTrigger>
+            <PopoverContent 
+                color='white' 
+                bg='#2f385f' 
+                borderColor='blue.800' 
+                width="400px" 
+                padding="10px 20px"
+
+            >
+            <PopoverHeader>
+                <div className='popover-header'>
+                    <h3>{anime.attributes.canonicalTitle} <span className="year-released">{anime.attributes.startDate.substring(0,4)}</span></h3>
+                </div>
+                <div className='popover-ratings'>
+                    <p className='popover-rating'>{anime.attributes.averageRating}%</p>
+                    <p className='popover-popular'><HeartOutlined style={{color: "#c92e2e"}} /> #{anime.attributes.popularityRank} Most Popular</p>
+                </div>
+            </PopoverHeader>
+            <PopoverArrow />
+            <PopoverBody>
+                <div className='popover-desc'>
+                    <p>{anime.attributes.synopsis.substring(0, 250)}...</p>
+                </div>
+            </PopoverBody>
+            <PopoverFooter
+                border='0'
+                display='flex'
+                alignItems='center'
+                justifyContent='space-between'
+                pb={4}
+            >
+            </PopoverFooter>
+            </PopoverContent>
+        </Popover>
+    </div>
   )
 }
 
