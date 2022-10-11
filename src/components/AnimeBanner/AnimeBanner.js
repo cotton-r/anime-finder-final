3dd32858022ef0f55eecb1867afb017b9f4ea639
cotton-r <66560099+cotton-r@users.noreply.github.com>
2022-10-11 17:47:59 +0100

    render anime icons

--- src/components/AnimeBanner/AnimeBanner.js
+++ src/components/AnimeBanner/AnimeBanner.js
@@ -4,7 +4,11 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useGetCategoryOfAnimeQuery } from '../../services/animeApi';
 import { addTrendingAnime } from './AnimeBannerSlice';
 
+import CircularProgress from "@mui/material/CircularProgress";
+
 import './AnimeBanner.css';
+
+import AnimeCard from '../AnimeCard/AnimeCard';
 
 const AnimeBanner = ({ category }) => {
 
@@ -17,10 +21,32 @@ const AnimeBanner = ({ category }) => {
         dispatch(addTrendingAnime(trendingAnime));
     }, [trendingAnime]);
 
-    console.log(animeData);
+    // hard coded array for placeholder loading symbol cards
+    const loadingCards = [1, 2, 3, 4, 5];
+    let incrementKey = 0;
 
   return (
-    <div>AnimeBanner</div>
+    <div className='container'>
+      {!isFetching
+        ? <div className="wrapper">
+            <div className='anime-list-wrapper'>
+              {animeData?.data?.map((anime) => (
+                <AnimeCard anime={anime} key={anime.id} />
+              ))}
+            </div>
+          </div>
+        :
+        <div className='wrapper'>
+            <div className='anime-list-wrapper'>
+                {loadingCards.map(() => (
+                    <div className='loading-card' key={incrementKey += 1} >
+                        <CircularProgress />
+                    </div>
+                ))}
+            </div>
+        </div>
+      }
+    </div>
   );
 };
 
---
+++ src/components/AnimeCard/AnimeCard.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+
+import './AnimeCard.css';
+
+import { Popover } from 'antd';
+
+const AnimeCard = ({ anime }) => {
+
+    const [show, setShow] = useState(false);
+
+    const popoverContent = (
+        <div className='popover-content'>
+            <div className='popover-header'>
+                <h3>{anime.attributes.canonicalTitle} <span className="year-released">{anime.attributes.startDate.substring(0,4)}</span></h3>
+            </div>
+            <div className='popover-ratings'>
+                <p>#{anime.attributes.popularityRank} Most Popular</p>
+                <p className='popover-rating'>{anime.attributes.averageRating}%</p>
+            </div>
+            <div className='popover-desc'>
+                <p>{anime.attributes.synopsis.substring(0, 250)}...</p>
+            </div>
+        </div>
+    )
+
+  return (
+    <Popover
+        overlayStyle={{maxWidth: '300px'}}
+        color="#2f385f"
+        content={popoverContent} 
+        Title={anime.attributes.canonicalTitle} 
+        placement="right"
+        mouseEnterDelay="0.3"
+        trigger="hover"
+        open={show}
+        onMouseLeave={() => setShow(false)}
+    >
+        <div className='anime-card'>
+            <a 
+                className='darken'
+                onMouseLeave={() => setShow(false)}
+                onMouseEnter={() => setShow(true)}
+            >
+                <img src={anime.attributes.posterImage.small} alt={anime.attributes.canonicalTitle} />
+            </a>
+        </div>
+    </Popover>
+  )
+}
+
+export default AnimeCard;
